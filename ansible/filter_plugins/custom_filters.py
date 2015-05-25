80cdf5aa251ee69c138df5969804e9a312961e9f
cewing <cris@crisewing.com>
2015-05-25 10:50:13 -0700

    provide docstring

--- ansible/filter_plugins/custom_filters.py
+++ ansible/filter_plugins/custom_filters.py
@@ -3,6 +3,7 @@ from jinja2.filters import environmentfilter, do_groupby
 
 @environmentfilter
 def do_dictby(environment, value, attribute):
+    """convert groups as built by groupby filter into a dictionary"""
     groups = do_groupby(environment, value, attribute)
     return dict(groups)
 
