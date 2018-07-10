58bf68dc0981b281bd776a4d02f61cd7b4b577eb
Daniel Roberts <droberts@dimagi.com>
2018-07-10 16:27:22 -0400

    WIP parallel playbooks

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -12,6 +12,39 @@ do :
 done
 
 commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
-commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
+
+
+commcare-cloud $ENV ansible-playbook update_apt_cache.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_common.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_lvm.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_db.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_commcarehq.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_proxy.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_shared_dir.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_webworker.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_formplayer.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_mailrelay.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_formplayer.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_formplayer.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_formplayer.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_formplayer.yml --skip-check --quiet --branch=$BRANCH
+commcare-cloud $ENV ansible-playbook deploy_formplayer.yml --skip-check --quiet --branch=$BRANCH
+
+
+wait
+
+- import_playbook: deploy_mailrelay.yml
+- import_playbook: deploy_tmpreaper.yml
+- import_playbook: deploy_etckeeper.yml
+- import_playbook: deploy_airflow.yml
+# migrate must happen before first fab, and also before touchforms can create its special user
+commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH --limit=couchdb2 &
+- import_playbook: migrate_on_fresh_install.yml
+- import_playbook: deploy_touchforms.yml
+- import_playbook: deploy_http_proxy.yml
+
+wait
+
+
 
 commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
