fce407bcd43b85a498302a5271e64211fb110e0a
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-08-10 17:50:49 -0400

    Lots of progress in python script, now need to separate it out into python commands

--- src/commcare_cloud/commands/ansible/run_module.py
+++ src/commcare_cloud/commands/ansible/run_module.py
@@ -213,25 +213,34 @@ class UpdateRiakSecrets(CommandBase):
                 ) + NON_POSITIONAL_ARGUMENTS
 
     def run(self, args, unknown_args):
-        print("GOT HERE")
 
-        # # Encrypt the vault file
+        # Encrypt the vault file
         subprocess.call("echo 123 | ansible-vault encrypt /Users/preethivaidyanathan/commcare-cloud/environments/pvtest/vault.yml --vault-password-file=/bin/cat", shell=True)
 
-        ## Define the key and secret to add
+        # Define the key and secret to add
         riak_key = subprocess.check_output("ssh 18.208.153.165 'cat /home/ansible/riak_creds.txt' | grep 'riak_key' | awk -F':' '{print $2}'",
-                                           shell=True)
+                                           shell=True).strip()
+
         riak_secret = subprocess.check_output(
             "ssh 18.208.153.165 'cat /home/ansible/riak_creds.txt' | grep 'riak_secret' | awk -F':' '{print $2}'",
-            shell=True)
-        #
-        ## Add the key and secret
-        # echo 123 | ansible-vault encrypt <( \
-        #  echo 123 | ansible-vault view vault.yml --vault-password-file=/bin/cat \
-        #    | python -c 'import yaml, sys;vars = yaml.load(sys.stdin); vars["secrets"]['"('$KEY')"'] = '"('$SECRET')"';print(yaml.safe_dump(vars, default_flow_style=False))'\
-        # ) --output vault.yml --vault-password-file=/bin/cat
-        #
-        # ## Decrypt the vault file
+            shell=True).strip()
+
+        print("===REACHED 3")
+        # Add the key and secret
+        print("++++++++++++++++++++")
+        py_cmd = "echo 123 | ansible-vault encrypt <(echo 123 | " \
+                 "ansible-vault view vault.yml --vault-password-file=/bin/cat | " \
+                 "python -c \'" \
+                 "import yaml, sys;" \
+                 "vars = yaml.load(sys.stdin); " \
+                 "vars[\"secrets\"][\"{}\"] = \"{}\";" \
+                 "print(yaml.safe_dump(vars, default_flow_style=False))\') " \
+                 "--output vault.yml --vault-password-file=/bin/cat".format(riak_key, riak_secret)
+
+        subprocess.call(py_cmd, shell=True)
+        print("===REACHED 4")
+
+        # Decrypt the vault file
         subprocess.call(
             "echo 123 | ansible-vault decrypt /Users/preethivaidyanathan/commcare-cloud/environments/pvtest/vault.yml --vault-password-file=/bin/cat",
             shell=True)
