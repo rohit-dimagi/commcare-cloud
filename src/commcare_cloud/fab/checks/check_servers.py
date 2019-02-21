215418c83b784e6382e2cfa01f89d52403e9e938
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-20 19:41:39 -0500

    mail and system checks

--- src/commcare_cloud/fab/checks/check_servers.py
+++ src/commcare_cloud/fab/checks/check_servers.py
@@ -56,6 +56,9 @@ def postgresql():
 @runs_once
 def perform_system_checks(current=False):
     path = env.code_current if current else env.code_root
-    venv = env.virtualenv_current if current else env.virtualenv_root  # TODO
+    if env.py3_deploy:
+        venv = env.py3_virtualenv_current if current else env.py3_virtualenv_root
+    else:
+        venv = env.virtualenv_current if current else env.virtualenv_root
     with cd(path):
         sudo('%s/bin/python manage.py check --deploy' % venv)
